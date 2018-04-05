a34027d93369b904f62349a4a92734b0c3166c71
Andrii Liashenko <>
2018-04-05 19:11:47 +0300

    [cleanup] Change format

--- shell.c
+++ shell.c
@@ -1,143 +1,138 @@
 /**
- * 
+ *
  *  shell
- *      author: Andrii Liashenko 
+ *      author: Andrii Liashenko
  *              ndrlyashenko@gmail.com
  *  shell has such builtins:
  *      help
  *      cd
  *      exit
  *  shell also supports linux commands as external executables.
- * 
+ *
  *  @TODO:
- *      ctrl-c should not kill the shell, but kill currently executing program if any executes
- *      quoting or backslash escaping
- *      globbing
- *      piping
- * 
+ *      change strtok to escape quotes
+ *      ctrl-c should not kill the shell, but kill currently executing program
+ *      piping (ls > out)
+ *      globbing (ls *.c)
  */
 #include "shell.h"
 
 #define BUFFER_SIZE 1024
 char prompt_text[BUFFER_SIZE];
 void set_prompt_text() {
-    getcwd(prompt_text, BUFFER_SIZE);
-    sprintf(prompt_text, "%s: > ", prompt_text);
+  getcwd(prompt_text, BUFFER_SIZE);
+  sprintf(prompt_text, "%s: > ", prompt_text);
 }
-char* read_input() {
-    char *input = readline(prompt_text);
-    add_history(input);
-    return input;
+char *read_input() {
+  char *input = readline(prompt_text);
+  add_history(input);
+  return input;
 }
 
 #define TOKENS_DELIMETERS " "
 #define TOKENS_BUFFER_SIZE 64
-char** parse_input(char* input) {
-    if (input == NULL) {
-        return NULL;
+char **parse_input(char *input) {
+  if (input == NULL) {
+    return NULL;
+  }
+  int capacity = TOKENS_BUFFER_SIZE;
+  char **tokens = malloc(capacity * sizeof(char *));
+  if (tokens == NULL) {
+    printf("shell: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  int size = 0;
+  char *token = strtok(input, TOKENS_DELIMETERS);
+  while (token) {
+    if (size >= capacity) {
+      printf("shell: command is too long\n");
+      free(token);
+      free(tokens);
+      return NULL;
     }
-    int capacity = TOKENS_BUFFER_SIZE;
-    char **tokens = malloc(capacity * sizeof(char*));
-    if (tokens == NULL) {
-        printf("shell: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-    int size = 0;
-    char *token = strtok(input, TOKENS_DELIMETERS);
-    while (token) {
-        if (size >= capacity) {
-            printf("shell: command is too long\n");
-            free(token);
-            free(tokens);
-            return NULL;
-        }
-        tokens[size++] = token;
-        token = strtok(NULL, TOKENS_DELIMETERS);
-    }
-    tokens[size] = NULL;
-    free(token);
-    return tokens;
+    tokens[size++] = token;
+    token = strtok(NULL, TOKENS_DELIMETERS);
+  }
+  tokens[size] = NULL;
+  free(token);
+  return tokens;
 }
 
-/**
- * Builtins
- */
-int shell_exit(char** argv) {
-    return 0;
-}
-int shell_help(char** argv) {
-    printf("\tshell\n");
-    printf("\tauthor: Andrii Liashenko\n");
-    printf("\t\tndrlyashenko@gmail.com\n\n");
-    printf("\tshell has such builtins:\n");
-    printf("\t\thelp\n");
-    printf("\t\tcd\n");
-    printf("\t\texit\n");
-    printf("\tshell also supports linux commands as external executables.\n");
+int execute(char **argv) {
+  if (argv == NULL || argv[0] == NULL) {
     return 1;
+  }
+  /** check is it builtin */
+  int i = 0;
+  while (builtins[i]) {
+    if (!strcmp(argv[0], builtins[i]->name)) {
+      return builtins[i]->func(argv);
+    }
+    ++i;
+  }
+  /** execute external executable program */
+  return execute_command(argv);
 }
-int shell_cd(char** argv) {
-    if (argv == NULL || argv[0] == NULL || argv[1] == NULL) {
-        return 1;
-    }
-    if (chdir(argv[1])) {
-        printf("shell: %s: No such file or directory\n", argv[1]);   
-        return 1;
-    }
-    set_prompt_text();
+/** Builtins */
+int shell_exit(char **argv) { return 0; }
+int shell_help(char **argv) {
+  printf("\tshell\n");
+  printf("\tauthor: Andrii Liashenko\n");
+  printf("\t\tndrlyashenko@gmail.com\n\n");
+  printf("\tshell has such builtins:\n");
+  printf("\t\thelp\n");
+  printf("\t\tcd\n");
+  printf("\t\texit\n");
+  printf("\tshell also supports linux commands as external executables.\n");
+  return 1;
+}
+int shell_cd(char **argv) {
+  if (argv == NULL || argv[0] == NULL || argv[1] == NULL) {
     return 1;
+  }
+  if (chdir(argv[1])) {
+    printf("shell: %s: No such file or directory\n", argv[1]);
+    return 1;
+  }
+  set_prompt_text();
+  return 1;
 }
 
-int execute(char** argv) {
-    if (argv == NULL || argv[0] == NULL) {
-        return 1;
+int execute_command(char **argv) {
+  if (argv == NULL) {
+    printf("shell: command not found\n");
+    return 1;
+  }
+  int status;
+  pid_t pid = fork();
+  if (pid < 0) {
+    printf("shell: forking child process failed\n");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+    /** child process */
+    if (execvp(argv[0], argv) < 0) {
+      printf("shell: %s: command not found\n", argv[0]);
+      exit(EXIT_FAILURE);
     }
-    // check builtins
-    int i = 0;
-    while (builtins[i]) {
-        if (!strcmp(argv[0], builtins[i]->name)) {
-            return builtins[i]->func(argv);
-        }
-        ++i;
-    }
-    // execute external executable program
-    return execute_command(argv);
-}
-// execute external executable
-int execute_command(char** argv) {
-    if (argv == NULL) {
-        printf("shell: command not found\n");
-        return 1;
-    }
-    int status;
-    pid_t pid = fork();
-    if (pid < 0) {
-        printf("shell: forking child process failed\n");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // child
-        if (execvp(argv[0], argv) < 0) {
-            printf("shell: %s: command not found\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-     } else {
-        // parent
-        while (wait(&status) != pid);
-     }
-     return 1;
+  } else {
+    /** parent process */
+    while (wait(&status) != pid)
+      ;
+  }
+  return 1;
 }
 
 int main() {
-    char *input = NULL;
-    char **argv = NULL;
-    int status = 1;
-    set_prompt_text();
-    do {
-        input = read_input();
-        argv = parse_input(input);
-        status = execute(argv);
-        free(input);
-        free(argv);
-    } while(status);
-    return EXIT_SUCCESS;
+  char *input = NULL;
+  char **argv = NULL;
+  int status = 1;
+  set_prompt_text();
+  do {
+    input = read_input();
+    argv = parse_input(input);
+    status = execute(argv);
+    free(input);
+    free(argv);
+  } while (status);
+  return EXIT_SUCCESS;
 }
--- shell.h
+++ shell.h
@@ -1,34 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
 
 /** Function declarations */
-char* read_input();
-char** parse_input(char* input);
-int execute(char** argv);
-int execute_command(char** argv);
+char *read_input();
+char **parse_input(char *input);
+int execute(char **argv);
+int execute_command(char **argv);
 
 /** Builtin functions */
-int shell_exit(char** argv);
-int shell_help(char** argv);
-int shell_cd(char** argv);
+int shell_exit(char **argv);
+int shell_help(char **argv);
+int shell_cd(char **argv);
 
 /** Builtins */
 struct builtin {
-    int (*func)(char**);
-    char *name;
+  int (*func)(char **);
+  char *name;
 };
 typedef struct builtin builtin;
-const static builtin builtin_exit = { &shell_exit, "exit" };
-const static builtin builtin_help = { &shell_help, "help" };
-const static builtin builtin_cd = { &shell_cd, "cd" };
-const static struct builtin *builtins[4] = {
-    &builtin_help,
-    &builtin_cd,
-    &builtin_exit,
-    NULL
-};
+const static builtin builtin_exit = {&shell_exit, "exit"};
+const static builtin builtin_help = {&shell_help, "help"};
+const static builtin builtin_cd = {&shell_cd, "cd"};
+const static struct builtin *builtins[4] = {&builtin_help, &builtin_cd,
+                                            &builtin_exit, NULL};
