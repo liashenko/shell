db2a2e554611b18995bcd6af5c3bbdd3e8658a4e
Andrii Liashenko <>
2018-04-10 18:11:25 +0300

    Add simple piping |

--- shell.c
+++ shell.c
@@ -7,33 +7,28 @@
  *      help
  *      cd
  *      exit
- *  shell also supports linux commands as external executables.
+ *  shell also supports external commands.
  *
  *  @TODO:
- *      piping (ls > out)
+ *      piping (>, <, >>, |)
  *      globbing (ls *.c)
- *      multiple commands &&, |
+ *      multiple commands &&
  */
 #include "shell.h"
 
-#define BUFFER_SIZE 1024
-char prompt_text[BUFFER_SIZE];
+char prompt_text[PROMPT_SIZE];
 void set_prompt_text() {
-  getcwd(prompt_text, BUFFER_SIZE);
+  getcwd(prompt_text, PROMPT_SIZE);
   sprintf(prompt_text, "%s: > ", prompt_text);
 }
+
 char *read_input() {
   char *input = readline(prompt_text);
   add_history(input);
   return input;
 }
 
-#define TOKENS_DELIMETERS " "
-#define TOKENS_BUFFER_SIZE 256
-#define TOKEN_SIZE 256
-int is_delimeter(char ch) { return strchr(TOKENS_DELIMETERS, ch) != NULL; }
-int is_quote(char ch) { return (ch == '\"' || ch == '\''); }
-char **parse_input(char *input) {
+char **parse_input(const char *input) {
   if (input == NULL) {
     return NULL;
   }
@@ -58,6 +53,10 @@ char **parse_input(char *input) {
     } else if (!is_in_quote && is_delimeter(input[input_i])) {
       if (token_i > 0) {
         tokens[tokens_number++] = strdup(token);
+        if (tokens_number > capacity) {
+          printf("shell: too long command\n");
+          return NULL;
+        }
         memset(token, 0, TOKEN_SIZE * sizeof(char));
         token_i = 0;
       }
@@ -78,6 +77,14 @@ int execute(char **argv) {
   if (argv == NULL || argv[0] == NULL) {
     return 1;
   }
+  int j = 0;
+  while (argv[j] != NULL) {
+    if (argv[j][0] == '|') {
+      return execute_pipe(argv);
+    }
+    if (argv[j] != NULL)
+    ++j;
+  }
   /** check if command is a builtin */
   int i = 0;
   while (i < builtins_number()) {
@@ -87,30 +94,6 @@ int execute(char **argv) {
     ++i;
   }
   return execute_external(argv);
-}
-/** Builtins */
-int shell_exit(char **argv) { return 0; }
-int shell_help(char **argv) {
-  printf("\tshell\n");
-  printf("\tauthor: Andrii Liashenko\n");
-  printf("\t\tndrlyashenko@gmail.com\n\n");
-  printf("\tshell has such builtins:\n");
-  printf("\t\thelp\n");
-  printf("\t\tcd\n");
-  printf("\t\texit\n");
-  printf("\tshell also supports linux commands as external executables.\n");
-  return 1;
-}
-int shell_cd(char **argv) {
-  if (argv == NULL || argv[0] == NULL || argv[1] == NULL) {
-    return 1;
-  }
-  if (chdir(argv[1])) {
-    printf("shell: %s: No such file or directory\n", argv[1]);
-    return 1;
-  }
-  set_prompt_text();
-  return 1;
 }
 
 int execute_external(char **argv) {
@@ -134,6 +117,81 @@ int execute_external(char **argv) {
     while (wait(&status) != pid)
       ;
   }
+  return 1;
+}
+
+int execute_pipe(char** argv) {
+    char **argv1 = malloc(TOKENS_BUFFER_SIZE * sizeof(char *));
+    char **argv2 = malloc(TOKENS_BUFFER_SIZE * sizeof(char *));
+    if (argv1 == NULL || argv2 == NULL) {
+      printf("shell: allocation error\n");
+      exit(EXIT_FAILURE);
+    }
+    int i = 0, j = 0;
+    while (argv[i][0] != '|') {
+      argv1[i] = argv[i];
+      ++i;
+    }
+    ++i;
+    while (argv[i] != NULL) {
+      argv2[j] = argv[i];
+      ++i, ++j;
+    }
+    int pipefd[2];
+    pipe(pipefd);
+    pid_t pid1 = fork();
+    if (pid1 < 0) {
+      printf("shell: forking child process failed\n");
+      exit(EXIT_FAILURE);
+    } else if (pid1 == 0) {
+      dup2(pipefd[1], 1);
+      close(pipefd[0]);
+      if (execvp(argv1[0], argv1) < 0) {
+        printf("shell: %s: command not found\n", argv1[0]);
+        exit(EXIT_FAILURE);
+      }
+    } 
+    pid_t pid2 = fork();
+    if (pid2 < 0) {
+      printf("shell: forking child process failed\n");
+      exit(EXIT_FAILURE);
+    } else if (pid2 == 0) {
+      dup2(pipefd[0], 0);
+      close(pipefd[1]);
+      if (execvp(argv2[0], argv2) < 0) {
+        printf("shell: %s: command not found\n", argv2[0]);
+        exit(EXIT_FAILURE);
+      }
+    }
+    close(pipefd[0]);
+    close(pipefd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    return 1;
+}
+
+/** Builtins */
+int shell_exit(char **argv) { return 0; }
+int shell_help(char **argv) {
+  printf("\tshell\n");
+  printf("\tauthor: Andrii Liashenko\n");
+  printf("\t\tndrlyashenko@gmail.com\n\n");
+  printf("\tshell has such builtins:\n");
+  printf("\t\thelp\n");
+  printf("\t\tcd\n");
+  printf("\t\texit\n");
+  printf("\tshell also supports external commands.\n");
+  return 1;
+}
+int shell_cd(char **argv) {
+  if (argv == NULL || argv[0] == NULL || argv[1] == NULL) {
+    return 1;
+  }
+  if (chdir(argv[1])) {
+    printf("shell: %s: No such file or directory\n", argv[1]);
+    return 1;
+  }
+  set_prompt_text();
   return 1;
 }
 
--- shell.h
+++ shell.h
@@ -7,12 +7,21 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
+#define TOKENS_DELIMETERS " "
+#define TOKENS_BUFFER_SIZE 256
+#define TOKEN_SIZE 256
+#define PROMPT_SIZE 1024
+
 /** Function declarations */
+void set_prompt_text();
 void sig_handler(int sig) {}
 char *read_input();
-char **parse_input(char *input);
+char **parse_input(const char *input);
+int is_delimeter(const char ch) { return strchr(TOKENS_DELIMETERS, ch) != NULL; }
+int is_quote(const char ch) { return (ch == '\"' || ch == '\''); }
 int execute(char **argv);
 int execute_external(char **argv);
+int execute_pipe(char** argv);
 
 /** Builtin functions */
 int shell_exit(char **argv);
@@ -20,10 +29,10 @@ int shell_help(char **argv);
 int shell_cd(char **argv);
 
 /** Builtins */
-struct builtin {
+typedef struct {
   char *name;
   int (*func)(char **);
-};
-const static struct builtin builtins[] = {
+} builtin;
+const static builtin builtins[] = {
     {"help", &shell_help}, {"cd", &shell_cd}, {"exit", &shell_exit}};
-int builtins_number() { return sizeof(builtins) / sizeof(struct builtin); }
+int builtins_number() { return sizeof(builtins) / sizeof(builtin); }
